b7f9e22a17039a1b0c12f6f0b412d0199af08598
irfana-lap <irfanaparvin905@gmail.com>
2026-04-07 14:17:54 +0530

    nine commit

---
+++ nine.js
@@ -0,0 +1,6 @@
+let number = 50; 
+if (number > 20 && number < 100) {
+    console.log(number + " lies between 20 and 100");
+} else {
+    console.log(number + " does NOT lie between 20 and 100");
+}
