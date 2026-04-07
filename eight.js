b3527ef109e8b47c09341421c98cf9578446800b
irfana-lap <irfanaparvin905@gmail.com>
2026-04-07 14:02:19 +0530

    eight commit

---
+++ eight.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = '10'; // Try changing this to 10 to see "Equal in both value and type"
+
+if (a === b) {
+    console.log("Equal in both value and type");
+} else {
+    console.log("Not equal in value or type");
+}
